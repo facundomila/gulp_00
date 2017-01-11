96593419a5928a4ec3e7cb16001296aab36daa0b
facumila <facundomm3@gmail.com>
2017-01-10 23:39:32 -0300

    environment

---
+++ src/app.js
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var App = React.createElement('h1', null, 'Hola mundoooooooooooo!');
+
+ReactDOM.render(App, document.getElementById('content'));
